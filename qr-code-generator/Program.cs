539bbebed4668c815bbf934e68594fa61515b7fe
Yurii  Bereza <y.bereza@pnn.in.ua>
2023-11-04 13:35:27 +0200

    Implemented base for generating QR code

---
+++ qr-code-generator/Program.cs
@@ -0,0 +1,13 @@
+﻿using qr_code_generator;
+
+Console.WriteLine("Enter your QR code text:");
+var qrCodeText = Console.ReadLine();
+
+if (string.IsNullOrEmpty(qrCodeText))
+{
+    Console.WriteLine("The entered text is empty.");
+    return;
+}
+
+new QRCodeGenerator().GenerateQRCode(qrCodeText, "res.png");
+Console.WriteLine("QR code successfully created and saved as 'res.png'");
---
+++ qr-code-generator/QRCodeGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System;
+using System.IO;
+using System.Xml;
+using System.Collections;
+using SkiaSharp;
+using System.Drawing;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using ZXing.SkiaSharp;
+
+namespace qr_code_generator
+{
+    public class QRCodeGenerator
+    {
+        public void GenerateQRCode(string content, string outputPath)
+        {
+            // Create a QR code writer
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+
+            // Set the encoding and error correction level
+            barcodeWriter.Options = new QrCodeEncodingOptions
+            {
+                DisableECI = true, // Disable ECI encoding
+                CharacterSet = "UTF-8",
+                ErrorCorrection = ErrorCorrectionLevel.H
+            };
+
+            // Generate the QR code as a BitMatrix
+            BitMatrix qrCodeMatrix = barcodeWriter.Encode(content);
+
+            // Convert the BitMatrix to a Bitmap
+            SKBitmap qrCodeBitmap = new SKBitmap(qrCodeMatrix.Width, qrCodeMatrix.Height);
+
+            for (int x = 0; x < qrCodeMatrix.Width; x++)
+            {
+                for (int y = 0; y < qrCodeMatrix.Height; y++)
+                {
+                    qrCodeBitmap.SetPixel(x, y, qrCodeMatrix[x, y] ? SKColors.Yellow : SKColors.Blue);
+                }
+            }
+
+            // saving image
+            using (var imageStream = new SKFileWStream(outputPath))
+            {
+                qrCodeBitmap.Encode(imageStream, SKEncodedImageFormat.Png, 1500);
+            }
+
+            // Dispose of the QR code Bitmap
+            qrCodeBitmap.Dispose();
+        }
+    }
+}
+
